9f246883a0f659301000bbffebbe1cb95c5f94f3
Fredrik <fredriep@fredriep-Precision-WorkStation-T5400.(none)>
2012-11-26 19:37:33 +0100

    ser ut som oppg d er bra

--- final_project/exc_cd.py
+++ final_project/exc_cd.py
@@ -9,21 +9,23 @@ domain_type = [UnitInterval,UnitSquare,UnitCube]
 mesh = domain_type[d](*divisions)
 
 def alpha(u):
-	return 1+u**2
+	return 1.0#+u**2
 
-def picard(u,u_1,a,L,bc,maxiter,tol=1e-5,order=2):
+def picard(u,u_1,a,L,b,maxiter,tol=1e-5,order=2):
 	iter = 0;eps = 1.0
 	while(eps>tol and iter<maxiter):
 		iter +=1
-		solve(a==L,u,bc)
+		#bc.apply(A,b)
+		solve(A,u.vector(),b)
 		diff = u.vector().array()-u_1.vector().array()
 		eps = np.linalg.norm(diff,ord=order)
 		u_1.assign(u)
+	#print iter
 	return None
 
-maxiter = 20
+maxiter = 1
 rho = 3.14
-dt = 0.1
+dt = 0.01
 V = FunctionSpace(mesh,'Lagrange',degree)
 f = Constant(0.0)
 u = TrialFunction(V)
@@ -35,26 +37,26 @@ u_1 = interpolate(u0,V)
 def u0_boundary(x,on_boundary):
 	return on_boundary
 
-bc = DirichletBC(V,u0,u0_boundary)
+#bc = DirichletBC(V,u0,u0_boundary)
 
 a = u*v*dx + dt*inner(alpha(u_1)*nabla_grad(u),nabla_grad(v))*dx
 L = (u_1 +dt*f)*v*dx
 
 A = assemble(a)
 u = Function(V)
-T = 1
+T = 0.5
 t = dt
 b = None
 exact = Expression('exp(-pi*pi*t)*cos(pi*x[0])',pi=pi,t=0)
 while t<=T:
+
 	b = assemble(L, tensor=b)
 	u0.t = t
-	bc.apply(A,b)
-	picard(u,u_1,a,L,bc,maxiter)
+	picard(u,u_1,a,L,b,maxiter)
 	#solve(A,u.vector(),b)
-	plot(u)
+	#plot(u)
 	#rescale=False
-	interactive()
+	#interactive()
 	t+=dt
 	u_1.assign(u)
 	exact.t=t
@@ -63,3 +65,5 @@ while t<=T:
 	#print 'Max error, t=%.2f: %-10.17f' % (t, maxdiff)
 	e = u_e.vector().array() - u.vector().array()
 	E = np.sqrt(np.sum(e**2)/u.vector().array().size)
+	if t==0.05:
+		print E/dt
